5ad2d79ce1cdcac7bbd7379597606d8a51dbb216
saidaLachgar <saidalachgar.dev@gmail.com>
2022-03-06 13:14:01 +0100

    - tp 14

--- Frontend/src/main/java/ma/cigma/Frontend/FrontendApplication.java
+++ Frontend/src/main/java/ma/cigma/Frontend/FrontendApplication.java
@@ -3,6 +3,8 @@ package ma.cigma.Frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -11,7 +13,10 @@ public class FrontendApplication {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
-
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
     public static void main(String[] args) {
         SpringApplication.run(FrontendApplication.class, args);
     }
---
+++ MavenProject/src/main/java/cigma/pfe/config/security/jwt.java
@@ -0,0 +1,60 @@
+package cigma.pfe.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+public class jwt {
+    // The secret key. This should be in a property file NOT under source
+    // control and not hard coded in real life. We're putting it here for simplicity.
+    private static String SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze";
+
+    public static void main(String[] args) {
+        String token = createJWT( "id", "issuer", "subject", 10000000l);
+        System.out.println(token);
+
+        Claims claims=decodeJWT(token);
+        System.out.println(claims);
+    }
+    //Sample method to construct a JWT
+    public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
+        // The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = SECRET_KEY.getBytes();
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+
+    public static Claims decodeJWT(String jwt) {
+        //This line will throw an exception if it is not a signed JWS (as expected)
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+}
